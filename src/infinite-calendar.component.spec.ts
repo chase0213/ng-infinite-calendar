c7725cb9266b169f295aca866a81996417864c42
Chisato HASEGAWA <chase0213@gmail.com>
2017-08-31 19:42:35 +0900

    setup for testing

--- src/infinite-calendar.component.spec.ts
+++ src/infinite-calendar.component.spec.ts
@@ -1,11 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By }              from '@angular/platform-browser';
 import { DebugElement }    from '@angular/core';
-import 'jasmine';
 
 import { InfiniteCalendarComponent } from './infinite-calendar.component';
 
-describe('SampleComponent', () => {
+describe('InfiniteCalendarComponent', () => {
 
   let comp:    InfiniteCalendarComponent;
   let fixture: ComponentFixture<InfiniteCalendarComponent>;
@@ -18,15 +17,14 @@ describe('SampleComponent', () => {
     });
 
     fixture = TestBed.createComponent(InfiniteCalendarComponent);
-
     comp = fixture.componentInstance; // BannerComponent test instance
 
     // query for the title <h1> by CSS element selector
-    de = fixture.debugElement.query(By.css('h1'));
-    el = de.nativeElement;
+    // de = fixture.debugElement.query(By.css('h1'));
+    // el = de.nativeElement;
   });
 
-  it('Should be false', () => {
-    expect(false).toBe(true);
+  it('Should be true', () => {
+    expect(true).toBe(true);
   });
 });
